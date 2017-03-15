dd6a140f0f0511372d2384d8b47e31f84a69ff3c
FHast <fritz.hast@gmail.com>
2017-03-15 16:53:56 +0100

    Creates JUnit test classes for all services

---
+++ ING/src/Services/test/JUnitTestCustomerAccount.java
@@ -0,0 +1,10 @@
+package Services.test;
+
+import Services.InvalidParameterException;
+import Services.TransactionService;
+
+public class JUnitTestTrnsaction {
+	public static void main(String[] args) {
+		
+	}
+}
